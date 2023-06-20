996115e61c8a6d79f7447d9b7aca55a2633f6fd0
takagi@test <taketo19960513@gmail.com>
2023-06-20 09:56:56 +0900

    first commit

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const express = require('express')
-const path = require('path')
+import express from 'express';
+import path from 'path'
 
 const PORT = process.env.PORT || 5001
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
+  .use(express.static('public'))
+  //  .use(express.static(path.join(__dirname, 'public')))
+  //.set('views', path.join(__dirname, 'views'))
+  //.set('view engine', 'ejs')
+  //.get('/', (req, res) => res.render('pages/index'))
   .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`))
